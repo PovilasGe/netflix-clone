b99a5a814d2573be06182b43dcfe3e0044fa07fe
PovilasGe <povilasgedminas@gmail.com>
2023-01-17 09:27:28 +0200

    netflix start

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSuSuZOVJl2iIQx__lsPcVh-AD0Px8moE",
+    authDomain: "netflix-clone-130eb.firebaseapp.com",
+    projectId: "netflix-clone-130eb",
+    storageBucket: "netflix-clone-130eb.appspot.com",
+    messagingSenderId: "708558580072",
+    appId: "1:708558580072:web:345e73a89a29b94099cedd"
+  };
