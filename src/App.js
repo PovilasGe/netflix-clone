42d0f7dcbf9ef9aa1b0832a6d6d08f4f274aeb43
PovilasGe <povilasgedminas@gmail.com>
2023-02-09 13:55:22 +0200

    add redux

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import HomeScreen from './screens/HomeScreen';
 import { Routes,  Route} from 'react-router-dom'
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
+import { useDispatch , useSelector} from 'react-redux';
+import { logout, login, selectUser } from './features/userSlice';
 
 function App() {
-  const user = undefined
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() =>{
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth){
         //logged in
-        console.log(userAuth);
+        dispatch(login({
+            uid: userAuth.uid,
+            email: userAuth.email
+          })
+        );
       } else {
         //logged out
+        dispatch(logout);
       }
     })
 
@@ -27,9 +35,13 @@ function App() {
         <Routes>
           {!user ? (
           <Route path='/' element={<LoginScreen />} />) : (
+          <>
           <Route path='/' element={<HomeScreen />} />
+          <Route path='/profile' element={<ProfileScreen />} />
+          </>
           )}
         </Routes>
+
 
     </div>
   );
